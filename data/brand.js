b8dc314393daa4fa89c7ae773802911b6837851e
gs_wenbing <gs_wenbing@sina.com>
2019-07-25 14:15:57 +0800

    readme

---
+++ data/brand.js
@@ -0,0 +1,9 @@
+brandList = [{
+		"CompanyBrandID": 1,
+		"CompanyBrandName": "金万年",
+		"PicURL": "http://res.genvana.cn/upload/goodsClassPic/201906030303004810.jpg",
+	},  {
+		"CompanyBrandID": 2,
+		"CompanyBrandName": "晨光",
+		"PicURL": "http://res.genvana.cn/upload/goodsClassPic/201906030338431295.jpg",
+	}]
