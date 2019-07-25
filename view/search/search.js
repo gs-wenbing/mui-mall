84a932fbe1c9939db74006a6512c24942c19acc7
gs_wenbing <gs_wenbing@sina.com>
2019-07-25 14:12:46 +0800

    商品搜索页面侧滑栏筛选

--- view/search/search.js
+++ view/search/search.js
@@ -1,4 +1,8 @@
+var main, menu, mask = mui.createMask(_closeMenu);
+var showMenu = false;
 mui.init({
+	swipeBack: false,
+	beforeback: back,
 	pullRefresh: {
 		container: '.mui-content',
 		down: {
@@ -17,6 +21,17 @@ mui.init({
 	}
 });
 
+function back() {
+	if (showMenu) {
+		//菜单处于显示状态，返回键应该先关闭菜单,阻止主窗口执行mui.back逻辑；
+		closeMenu();
+		return false;
+	} else {
+		//菜单处于隐藏状态，执行返回时，要先close菜单页面，然后继续执行mui.back逻辑关闭主窗口；
+		menu.close('none');
+		return true;
+	}
+}
 
 mui.plusReady(function() {
 	if (isIPhoneX()) {
@@ -34,18 +49,33 @@ mui.plusReady(function() {
 			height: height
 		});
 	}
-	var self = plus.webview.currentWebview();
-	var keyWords = self.keyWords;
+	
+	//plusReady事件后，自动创建menu窗口；
+	main = plus.webview.currentWebview();
+	//setTimeout的目的是等待窗体动画结束后，再执行create webview操作，避免资源竞争，导致窗口动画不流畅；
+	setTimeout(function() {
+		menu = mui.preload({
+			id: 'class-menu',
+			url: 'class-menu.html',
+			styles: {
+				left: "20%",
+				width: '80%',
+				zindex: 9997
+			}
+		});
+	}, 500);
+	//加载数据
 	pulldownRefresh();
 	GoodsSearch.$nextTick(function() {
 		showWindow();
-		// mui('#offCanvasContentScroll').scroll().scrollTo(0, 0, 100);
 	})
 });
 
 var pageIndex =0;
 var pageSize = 10;
-//下拉刷新
+/**
+ * 下拉刷新
+ */
 function pulldownRefresh() {
 	pageIndex = 0;
 	$(".underline").css('display', 'none');
@@ -54,16 +84,16 @@ function pulldownRefresh() {
 	}
 	window.setTimeout(function() {
 		GoodsSearch.MallGoodsList = goodsList[pageIndex];
-		// GoodsSearch.BrandList = data.BrandList;
-		GoodsSearch.ClassList = classList;
-		mui(".mui-content").pullRefresh().endPulldownToRefresh();
+		if(mui('.mui-content').pullRefresh()){
+			mui(".mui-content").pullRefresh().endPulldownToRefresh();
+		}
 		pageIndex++;
-		console.log(pageIndex+"===");
 	}, 500);
 }
-//上拉加载更多
+/**
+ * 上拉加载更多
+ */
 function pullupLoading(){
-	console.log(pageIndex+"==="+goodsList.length);
 	if(pageIndex===goodsList.length){
 		return false;
 	}
@@ -82,11 +112,9 @@ function pullupLoading(){
 	}, 500);
 }
 var GoodsSearch = new Vue({
-	el: '.mui-content',
+	el: '#app',
 	data: {
-		MallGoodsList: [],
-		BrandList: [],
-		ClassList: [],
+		MallGoodsList: []
 	},
 	computed: {
 		classObject: function(index) {
@@ -108,33 +136,93 @@ var GoodsSearch = new Vue({
 	methods: {
 		//关键字搜索
 		searchKey: function() {
-			search();
-		},
-		//重置
-		reSetSearch: function() {
-			$(".mui-selected").removeClass("mui-selected");
-			$(".btns-item").removeClass("active");
-			$('.widget-body__offCanvasSide1').html('');
-			$('.widget-body__offCanvasSide2').html('');
-			document.activeElement.blur(); //隐藏软键盘  
-		},
-		//确定
-		queRsearch: function() {
-			document.activeElement.blur(); //隐藏软键盘  
-			mui('#offCanvasWrapper').offCanvas('close');
-			search();
+			filterGoods();
 		},
 		gotoGoodsDetail: function(goodsId) {
 			createGoodsDetail(goodsId);
 		}
 	}
 });
-
-function search() {
-	pageIndex = 1;
-	getMallGoodsList();
+/**
+ * 筛选商品
+ */
+function filterGoods(){
+	//模拟
+	var wd = plus.nativeUI.showWaiting();
+	window.setTimeout(function() {
+		wd.close();
+	}, 1000);
 }
 
+/*
+ * 显示菜单菜单
+ */
+function openMenu() {
+	if (!showMenu) {
+		//解决android 4.4以下版本webview移动时，导致fixed定位元素错乱的bug;
+		if (mui.os.android && parseFloat(mui.os.version) < 4.4) {
+			document.querySelector("header.mui-bar").style.position = "static";
+			//同时需要修改以下.mui-contnt的padding-top，否则会多出空白；
+			document.querySelector(".mui-bar-nav~.mui-content").style.paddingTop = "0px";
+		}
+		//侧滑菜单处于隐藏状态，则立即显示出来；
+		//显示完毕后，根据不同动画效果移动窗体；
+		menu.show('none', 0, function() {
+			menu.setStyle({
+				left: '20%',
+				transition: {
+					duration: 150
+				}
+			});
+		});
+		//显示主窗体遮罩
+		mask.show();
+		showMenu = true;
+	}
+}
+
+function closeMenu(e) {
+	//窗体移动
+	_closeMenu(e);
+	//关闭遮罩
+	mask.close();
+}
+
+/**
+ * 关闭侧滑菜单(业务部分)
+ */
+function _closeMenu(e) {
+	if (showMenu) {
+		//解决android 4.4以下版本webview移动时，导致fixed定位元素错乱的bug;
+		if (mui.os.android && parseFloat(mui.os.version) < 4.4) {
+			document.querySelector("header.mui-bar").style.position = "fixed";
+			//同时需要修改以下.mui-contnt的padding-top，否则会多出空白；
+			document.querySelector(".mui-bar-nav~.mui-content").style.paddingTop = "44px";
+		}
+		//主窗体开始侧滑；
+		menu.setStyle({
+			left: '100%',
+			transition: {
+				duration: 150
+			}
+		});
+		//等窗体动画结束后，隐藏菜单webview，节省资源；
+		setTimeout(function() {
+			menu.hide();
+		}, 300);
+		showMenu = false;
+		//接受选择参数
+		if(e && e.detail){
+			//价格、品牌、类型、分类
+			console.log(JSON.stringify(e.detail));
+			//筛选
+			filterGoods();
+		}
+	}
+}
+document.getElementById("show-btn").addEventListener('tap', openMenu);
+//关闭菜单；
+window.addEventListener("menu:swiperight", closeMenu);
 
 function IPhoneXCSS(){
 	if(plus.navigator.isImmersedStatusbar() && isIPhoneX()){
