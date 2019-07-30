3ae3918aa8d700af71261340639f8de120016648
gs_wenbing <gs_wenbing@sina.com>
2019-07-30 08:50:28 +0800

    商品详情

--- data/detail.js
+++ data/detail.js
@@ -1,18 +1,14 @@
 detail = {
 	"Goods": {
 		"GoodsID": 49,
-		"GoodsCode": "JWN20181025115605884",
 		"GoodsName": "牛滑木色杆1.0MM半针配RS02系列芯拔帽中油笔",
 		"GoodsModel": "G-3126",
-		"CompanyName": "广东金万年文具有限公司",
 		"DefaultPicURL": "http://res.genvana.cn/upload/goods/201906161026374064.jpg",
-		"GoodsClassName": "中油笔",
-		"CreateTimeStr": "2018-10-25 11:56:05",
 		"GoodsSalePoint": "超级好用",
-		"GoodsStatusName": "在售",
+		"GoodsTag":"新品,热销",
+		"ReductionStr":"购买该商品满50元减5元,满100元减10元，满100元减10元，满100元减10元，满100元减10元",
+		"GoodsUnitTemplateDes":"1245PCS/箱，145PCS/盒，1PCS/PCS",
 		"GoodsDesc": "%3Cp%3E%3Cimg%20src%3D%22http%3A%2F%2F%7BIMGIP%7D%2Fupload%2Fgoods%2F201906161027479513.jpg%22%20title%3D%22G-3126%E8%AF%A6%E6%83%85_03.jpg%22%2F%3E%3C%2Fp%3E%3Cp%3E%3Cimg%20src%3D%22http%3A%2F%2F%7BIMGIP%7D%2Fupload%2Fgoods%2F201906161027487433.jpg%22%20title%3D%22G-3126%E8%AF%A6%E6%83%85_01.jpg%22%2F%3E%3C%2Fp%3E%3Cp%3E%3Cimg%20src%3D%22http%3A%2F%2F%7BIMGIP%7D%2Fupload%2Fgoods%2F201906161027493361.jpg%22%20title%3D%22G-3126%E8%AF%A6%E6%83%85_02.jpg%22%2F%3E%3C%2Fp%3E%3Cp%3E%3Cbr%2F%3E%3C%2Fp%3E",
-		"GoodsUnitTemplateName": "12*12*12=1728",
-		"GoodsUnitTemplateDes": "1728PCS/箱,144PCS/条,12PCS/盒,1PCS/PCS",
 	},
 	"GoodsSku": [{
 		"GoodsAttributesValueName": "黑色",
